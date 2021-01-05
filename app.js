9c760308c84b55869122c42a5838f91bad05395c
Erin Schippers <52472490+eschipp@users.noreply.github.com>
2021-01-04 23:07:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function computerPlay(){
   let options = ['rock', 'paper', 'scissors']
-  return options[Math.floor(Math.random() * options.length])
+  return options[Math.floor(Math.random() * options.length]
   }
   
 function playRound(playerSelection, computerSelection) {
